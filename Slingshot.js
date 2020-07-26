d992a6dca681f6870ee1816ea958867ea185fe1d
agrawalmehal3 <agrawalmehal3>
2020-07-26 16:54:00 -0700

    completed Catapult

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.slingshot1= loadImage("sprites/sling1.png");
+        this.slingshot2= loadImage("sprites/sling2.png");
+        this.slingshot3= loadImage("sprites/sling3.png");
+
     }
 
     fly(){
@@ -16,11 +20,26 @@ class SlingShot{
     }
 
     display(){
+        image(this.slingshot1,200,20);
+        image(this.slingshot2, 170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            stroke (49,22,8);
+            if (pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x - 25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-25,pointA.y,pointB.x + 30,pointB.y);
+                image(this.slingshot3, pointA.x-25, pointA.y-10,15,30);
+            }
+            else {
+                strokeWeight(4);
+                line(pointA.x + 25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x + 25,pointA.y,pointB.x + 30,pointB.y);
+                image(this.slingshot3, pointA.x +25, pointA.y-10,15,30);
+            }
+           
         }
     }
     
